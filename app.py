990275c37b03bbcc59af07f1d7b1d55fde881b77
Angel Perez <angel.jperez@gmail.com>
2026-03-08 14:15:16 -0300

    metricas de disponibilidad y FCST

--- app.py
+++ app.py
@@ -1,13 +1,125 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+
 from data_access import get_disponibilidad
 
-st.title("Supply Dashboard")
+#falta agregar
+    #distribucion de inventario por subfamilia.
+    #primeros 20 sku tip 1 y 2 de la categoria.
 
+# Layout ancho
+st.set_page_config(layout="wide")
 
-st.title("Control Tower Supply")
-st.header("Disponibilidad - Familia Muebles y Organización")
+st.title("Disponibilidad - Familia Muebles y Organización")
 
 df = get_disponibilidad()
 
-st.dataframe(df)
+# ---------------- KPI ----------------
+
+disponibilidad = df["disponibilidad_fcst"].mean()
+riesgo = (df["alerta_abastecimiento"] == "RIESGO QUIEBRE").mean()*100
+fcst_error = (df["obs_fcst"] != "FCST OK").mean()*100
+cobertura = df["sem_cobertura"].mean()
+
+col1, col2, col3, col4 = st.columns(4)
+
+col1.metric("Disponibilidad promedio", round(disponibilidad,2))
+col2.metric("% Riesgo quiebre", round(riesgo,2))
+col3.metric("% Forecast incorrecto", round(fcst_error,2))
+col4.metric("Cobertura semanas", round(cobertura,2))
+
+st.divider()
+
+# ---------------- ANALISIS GENERAL ----------------
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Estado Forecast")
+    st.bar_chart(df["obs_fcst"].value_counts())
+
+with col2:
+    st.subheader("Disponibilidad promedio por tienda")
+    disp_tienda = df.groupby("id_tienda")["disponibilidad_fcst"].mean()
+    st.bar_chart(disp_tienda)
+
+st.divider()
+
+# ---------------- ANALISIS DEMANDA ----------------
+
+col3, col4 = st.columns(2)
+
+with col3:
+
+    st.subheader("Forecast vs Ventas")
+
+    fig = px.scatter(
+        df,
+        x="fcst",
+        y="PV6",
+        color="alerta_abastecimiento",
+        hover_data=["sku","id_tienda"]
+    )
+
+    st.plotly_chart(fig, use_container_width=True)
+
+with col4:
+
+    st.subheader("Cobertura en semanas")
+
+    fig_hist = px.histogram(
+        df,
+        x="sem_cobertura",
+        nbins=30
+    )
+
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+st.divider()
+
+# ---------------- ALERTAS ----------------
+
+st.subheader("Alertas de abastecimiento")
+
+alertas = df[df["alerta_abastecimiento"] != "OK"]
+
+st.dataframe(
+    alertas,
+    use_container_width=True
+)
+
+st.divider()
+
+# ---------------- SKU CRITICOS ----------------
+
+st.subheader("SKUs con Alerta de Reposicion")
+
+problemas = df[
+    (df["alerta_abastecimiento"] == "RIESGO QUIEBRE") |
+    (df["obs_fcst"] != "FCST OK")
+]
+
+problemas = problemas.sort_values("PV6", ascending=False)
+
+st.dataframe(
+    problemas[[
+        "id_tienda",
+        "sku",
+        "descripcion_producto",
+        "stock_total",
+        "fcst",
+        "PV6",
+        "sem_cobertura",
+        "obs_fcst",
+        "alerta_abastecimiento"
+    ]],
+    use_container_width=True
+)
+
+st.subheader("Top 20 SKUs más críticos por venta")
+
+st.dataframe(
+    problemas.nlargest(20, "PV6"),
+    use_container_width=True
+)
--- data_access.py
+++ data_access.py
@@ -1,17 +1,20 @@
 import mysql.connector
 import pandas as pd
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
 
     conexion = mysql.connector.connect(
-        host ="ciclope.sodimac.cl",
-        user ="ajperez",
-        password = "W?K=r4mww",
-        database = "sodimac_grt"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
     return conexion
-
 # define("SERVER","ciclope.sodimac.cl");
 # define("USUARIO","ajperez");
 # define("PASSWORD","W?K=r4mww");
@@ -22,42 +25,17 @@ def get_connection():
 def get_disponibilidad() :
 
     conn = get_connection()
+    cursor = conn.cursor()
 
-    query = """SELECT 
-                vta.id_tienda,
-                pm.sku,
-                pm.descripcion_producto,
-                pm.id_familia,
-                pm.id_sub_familia,
-                (GREATEST(inv.saldo_disponible,0) + inv.saldo_pendiente) as stock_total,
-                comp.fcst1 as fcst,
-                comp.lead_time,
-                ROUND((GREATEST(inv.saldo_disponible,0) + inv.saldo_pendiente) / comp.fcst1,2) as disponibilidad_fcst,
-                ROUND(((GREATEST(inv.saldo_disponible,0) + inv.saldo_pendiente) /(comp.fcst1 / 7 *comp.lead_time)) * 100,2) as disp_lt,
-                ass.assorment
+    query = """ CALL Analisis_Procesos.sp_dashboard_python() """
+    cursor.execute(query)
 
-                FROM sodimac_grt.productos_maestro as pm 
+    columns = [col[0] for col in cursor.description]
+    data = cursor.fetchall()
 
-                LEFT JOIN sodimac_grt.ventas_detalle_semanal_L6W as vta  on pm.sku = vta.sku
-                LEFT JOIN relex.comparativo_RO as comp on  vta.id_tienda = comp.id_tienda and pm.sku = comp.sku
-                LEFT JOIN sodimac_grt.assorment_sku_tiendas_no as ass on vta.id_tienda = ass.id_tienda and pm.sku = ass.sku
-                LEFT JOIN sodimac_grt.MID_sku_tiendas as mid on vta.id_tienda = mid.id_tienda and pm.sku = mid.sku
-                LEFT JOIN sodimac_grt.MAEMED as mae on  pm.sku = mae.SKU
-
-
-                LEFT JOIN (SELECT 
-                                                            inv.sku,
-                                                            SUM(inv.saldo_disponible) as disp_bod,
-                                                            SUM(inv.saldo_pendiente) as pend_bod
-                                                        FROM sodimac_grt.inventario_diario as inv 
-                                                        INNER JOIN sodimac_grt.bodegas as b on inv.id_sucursal = b.id_bodega and b.asigna = 1
-                                                        GROUP BY 
-                                                        inv.sku) as invb on pm.sku = invb.sku
-                LEFT JOIN sodimac_grt.inventario_diario as inv on vta.id_tienda = inv.id_sucursal and pm.sku = inv.sku
-
-                WHERE pm.id_familia = 417 and pm.id_comprador_producto not in (1,69,39,15,18) and ass.assorment = 'SI'
-    """
-    df = pd.read_sql(query,conn)
+    df = pd.DataFrame(data, columns=columns)
+    
+    cursor.close()
     conn.close()
 
     return df
